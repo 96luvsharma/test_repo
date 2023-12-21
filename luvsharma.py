ec7bbdb07e61729b842ec41b79dcc52a7315c399
Luv Sharma <96482088+96luvsharma@users.noreply.github.com>
2023-12-21 15:36:18 +0100

    Update luvsharma.py

--- luvsharma.py
+++ luvsharma.py
@@ -35,7 +35,7 @@ markets = ["Select a market to get the stock tickers","S&P500","NASDAQ", "DAX40"
 
 nav = option_menu(menu_title = 'Navigation',
     options=['Efficient Frontier']#,'Multiple Firm F-Score', 'Single Firm F-Score','Portfolio Dashboard', 'About Me' ],
-    icons=['pie-chart-fill','cash-coin','file-bar-graph-fill','file-binary-fill','file-person'],
+    icons=['pie-chart-fill']#,'cash-coin','file-bar-graph-fill','file-binary-fill','file-person'],
     menu_icon= "list",
     default_index=0,
     orientation= 'horizontal',
